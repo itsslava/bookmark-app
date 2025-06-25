46462413d3ed54576ef4bab1f77d4cd30617f369
itsslava <77506882+itsslava@users.noreply.github.com>
2025-06-25 17:49:54 +0300

    feat(app): fetch and display profile in navigation

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import ProfileAvatar from './components/profile-avatar.vue';
+import { API_ROUTES } from './api';
+import { Profile } from './interfaces/profile.ts';
+
+const profile = ref<Profile>();
+
+async function fetchProfile() {
+  const data = await fetch(API_ROUTES.profile);
+  const res = (await data.json()) as Profile;
+  profile.value = res;
+}
+
+onMounted(() => {
+  fetchProfile();
+});
+</script>
 
 <template>
   <div class="app">
-    <nav class="nav">Меню</nav>
+    <nav class="nav"><ProfileAvatar v-if="profile" :name="profile.name" /></nav>
     <main>Контент</main>
   </div>
 </template>
@@ -13,7 +30,8 @@
   min-height: calc(100vh - 140px);
   gap: 200px;
   max-width: 1450px;
-  margin: 140px auto 0 auto;
+  padding: 140px 120px;
+  /* margin: 140px auto 0 auto; */
 }
 .nav {
   min-width: 400px;
