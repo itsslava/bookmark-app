c98c597a3183d1e6c6a719b4fc8428ad9937e1d0
itsslava <77506882+itsslava@users.noreply.github.com>
2025-06-25 17:49:48 +0300

    feat(profile-avatar): create avatar component with name prop

---
+++ src/components/profile-avatar.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+const { name } = defineProps<{ name: string }>();
+</script>
+<template>
+  <div class="avatar">
+    <img class="avatar_img" src="/public/avatar.png" />
+    <div class="avatar_text">
+      Hello, <span>{{ name }}!</span>
+    </div>
+  </div>
+</template>
+<style scoped>
+.avatar {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start; /* сдвигаем всё содержимое к левому краю */
+  gap: 24px;
+  text-align: left;
+}
+.avatar_img {
+  width: 80px;
+  height: 80px;
+}
+.avatar_text {
+  font-weight: 400;
+  font-size: 18px;
+  letter-spacing: 2%;
+  text-align: left;
+}
+.avatar_text span {
+  font-weight: 700;
+}
+</style>
