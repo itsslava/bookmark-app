616ad63a646bccf90f65d42330a39b0fe6f4dfd4
itsslava <77506882+itsslava@users.noreply.github.com>
2025-06-25 13:06:34 +0300

    chore(project): initialize Vue application

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="app">
+    <nav class="nav">Меню</nav>
+    <main>Контент</main>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  display: flex;
+  min-height: calc(100vh - 140px);
+  gap: 200px;
+  max-width: 1450px;
+  margin: 140px auto 0 auto;
+}
+.nav {
+  min-width: 400px;
+}
+</style>
