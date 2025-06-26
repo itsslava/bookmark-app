e13212d74c9ea3d506e1277e96e1c3b41deb901c
itsslava <77506882+itsslava@users.noreply.github.com>
2025-06-26 17:12:12 +0300

    feat(index-view): add welcome message and layout styling

--- src/views/index-view.vue
+++ src/views/index-view.vue
@@ -1,1 +1,28 @@
-<template>Основная страница</template>
+<template>
+  <div class="index">
+    <h1 class="index-title">Welcome to Bookmarkly!</h1>
+    <p class="index-description">
+      To start using the app, select a bookmark category or create a new one.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.index {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 25px;
+  min-height: 50vh;
+}
+
+.index-title {
+  font-weight: 500;
+  font-size: 24px;
+}
+.index-description {
+  font-weight: 400;
+  font-size: 16px;
+}
+</style>
