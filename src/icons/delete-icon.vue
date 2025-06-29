b9d131a856f5b7b47dbb512d8580bc7a0e464940
itsslava <77506882+itsslava@users.noreply.github.com>
2025-06-29 16:15:24 +0300

    feat(icons): add EditIcon, OkIcon and DeleteIcon components

---
+++ src/icons/delete-icon.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M5.34961 2.33333C5.58985 1.65364 6.23807 1.16667 7.00003 1.16667C7.76199 1.16667 8.41021 1.65364 8.65045 2.33333"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+    />
+    <path d="M11.9582 3.5H2.0415" stroke="white" stroke-width="1.5" stroke-linecap="round" />
+    <path
+      d="M10.9861 4.95833L10.7178 8.98282C10.6146 10.5315 10.563 11.3059 10.0584 11.7779C9.55379 12.25 8.77773 12.25 7.22559 12.25H6.77446C5.22233 12.25 4.44626 12.25 3.94167 11.7779C3.43708 11.3059 3.38546 10.5315 3.28221 8.98282L3.01392 4.95833"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+    />
+    <path
+      d="M5.54175 6.41667L5.83342 9.33333"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+    />
+    <path
+      d="M8.45841 6.41667L8.16675 9.33333"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
