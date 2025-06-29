8a268b6254049fc0955e6c6f758a4d6fb8de7040
itsslava <77506882+itsslava@users.noreply.github.com>
2025-06-29 13:14:08 +0300

    feat(auth-ui): add InputString component and integrate login form

---
+++ src/components/input-string.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+const data = defineModel<string>();
+</script>
+<template><input class="input-string" v-model="data" /></template>
+<style scoped>
+.input-string {
+  border: none;
+  border-bottom: 1px solid var(--color-fg);
+  min-width: 274px;
+  font-size: 24px;
+}
+.input-string::placeholder {
+  color: var(--color-inactive);
+}
+</style>
--- src/views/auth-view.vue
+++ src/views/auth-view.vue
@@ -1,17 +1,31 @@
 <script lang="ts" setup>
-import { useRouter } from 'vue-router';
 import ButtonText from '../components/button-text.vue';
+import InputString from '@/components/input-string.vue';
+import { useAuthStore } from '@/stores/auth.store';
+import { ref } from 'vue';
 
-const router = useRouter();
-const redirectToMain = () => {
-  router.push({ name: 'main' });
-};
+const form = ref<{ email?: string; password?: string }>({});
+const authStore = useAuthStore();
+
+function submitForm(event: Event) {
+  event.preventDefault();
+  if (!form.value.email || !form.value.password) {
+    return;
+  }
+  authStore.login(form.value.email, form.value.password);
+  // reset form after submission
+  form.value = {};
+}
 </script>
 <template>
   <div class="auth">
     <div class="auth-form">
       <h1 class="auth-title">Bookmarkly</h1>
-      <ButtonText @click="redirectToMain">Log in</ButtonText>
+      <form class="auth-form" @submit="submitForm">
+        <InputString v-model="form.email" placeholder="Email" />
+        <InputString v-model="form.password" placeholder="Password" type="password" />
+        <ButtonText type="submit">Log in</ButtonText>
+      </form>
     </div>
   </div>
 </template>
