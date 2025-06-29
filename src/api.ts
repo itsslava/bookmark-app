1e93130137fa16f30f96b57e32801126205cb69e
itsslava <77506882+itsslava@users.noreply.github.com>
2025-06-29 13:13:56 +0300

    feat(api & auth-store): add login route and authentication store

--- src/api.ts
+++ src/api.ts
@@ -4,6 +4,9 @@ export const API_ROUTES = {
   profile: `profile`,
   categories: 'categories',
   bookmarks: (id: number) => `categories/${id}/bookmarks`,
+  auth: {
+    login: 'auth/login',
+  },
 };
 
 export const http = axios.create({ baseURL: 'http://localhost:3000/api/', timeout: 10000 });
