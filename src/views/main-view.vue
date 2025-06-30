73aafe99da0a63f9590367fd20e4e2dd6da690f7
itsslava <77506882+itsslava@users.noreply.github.com>
2025-06-30 16:39:21 +0300

    style(base & main-view): update color palette and layout padding

--- src/views/main-view.vue
+++ src/views/main-view.vue
@@ -15,10 +15,9 @@ import ProfileAvatar from '../components/profile-avatar.vue';
 .app {
   display: flex;
   min-height: calc(100vh - 140px);
-  gap: 200px;
+  /* gap: 200px; */
   max-width: 1450px;
-  padding: 140px 120px;
-  /* margin: 140px auto 0 auto; */
+  padding: 40px;
 }
 .nav {
   min-width: 320px;
